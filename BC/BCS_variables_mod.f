07951c5e1ee04417e317c9b44b3d69d0db548511
lchacon <devnull@localhost>
2005-01-25 22:04:56 +0000

    * Added BCS_variables_mod.f

---
+++ BC/BCS_variables_mod.f
@@ -0,0 +1,36 @@
+c module BCS_variables
+c####################################################################
+      module BCS_variables
+
+        use grid
+
+        use grid_aliases
+
+        use auxiliaryVariables
+
+        use operators
+
+        use icond
+
+        use equilibrium
+
+        use transport_params
+
+        use constants
+
+        use variables
+
+        use error
+
+        use generalPurposeFunctions
+
+        integer(4) :: nnvar,imax,imin,jmax,jmin,kmax,kmin
+        integer(4) :: nxbc,nybc,nzbc,igxbc,igybc,igzbc
+
+        real(8),allocatable,dimension(:,:) :: rhs
+
+        real(8),allocatable,dimension(:,:,:,:) :: v_cov,v_cnv,v0
+
+        logical :: symm=.false.
+
+      end module BCS_variables
