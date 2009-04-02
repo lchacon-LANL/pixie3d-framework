911f8c78730c2e94d5a7980cd6909620fd0cd4aa
lch <lch@quevedo.ornl.gov>
2009-04-02 17:34:18 -0400

    * Recovered some of the changes made in earlier versions.

--- matrixLightSolver/mlsolver_mod.f
+++ matrixLightSolver/mlsolver_mod.f
@@ -28,26 +28,38 @@ c######################################################################
           logical    :: singular_matrix
 
           !MG quantities
-          integer      :: igridmin
+          integer      :: mg_gmin
           integer      :: vcyc
           integer      :: orderres
           integer      :: orderprol
           integer      :: mg_coarse_solver_depth
           integer      :: mg_mu
+
+          logical      :: mg_line_relax
           integer      :: mg_line_nsweep
           integer      :: mg_line_vcyc
           integer      :: mg_line_coarse_solver_depth
-          logical      :: mg_line_relax
           logical      :: mg_line_x
           logical      :: mg_line_y
           logical      :: mg_line_z
           character(2) :: mg_line_solve
           real(8)      :: mg_line_tol
           real(8)      :: mg_line_omega
-          logical      :: vertex_based_relax
-          logical      :: galerkin
+
+          logical      :: mg_vertex_relax
+
+          logical      :: mg_zebra_relax
+          integer      :: mg_zebra_prefd
+          integer      :: mg_zebra_it
+          real(8)      :: mg_zebra_omega
+
+          logical      :: mg_galerkin
+          logical      :: mg_debug
+
 
           type(grid_mg_def),pointer  :: mg_grid_def
+
+          type(mg_ctx),pointer :: mgctx
 
           !Output quantities
           integer    :: iter_out
@@ -120,7 +132,7 @@ c       Initializes solver options
 
           !MG and smoother options
           solverOptions%vcyc     = 1               !Number of V-cycles (MG)
-          solverOptions%igridmin = 2               !Minimum grid level considered (MG)
+          solverOptions%mg_gmin  = 2               !Minimum grid level considered (MG)
           solverOptions%orderres = 0               !Interpolation order in restriction (MG)
           solverOptions%orderprol= 0               !Interpolation order in prolongation (MG)
           solverOptions%fdiag    = .true.          !Whether to form matrix diagonal
@@ -149,10 +161,16 @@ c       Initializes solver options
           solverOptions%mg_line_y      = .true.    !Whether to do lines in y-direction
           solverOptions%mg_line_z      = .true.    !Whether to do lines in z-direction
 
-          solverOptions%vertex_based_relax=.false. !Whether to do vertex-based relax.
+          solverOptions%mg_vertex_relax= .false.   !Whether to do vertex-based relax.
 
-          solverOptions%galerkin    =.false.       !Whether to do Galerkin coarsening (true)
+          solverOptions%mg_zebra_relax = .false.   !Whether to do zebra relax.
+          solverOptions%mg_zebra_prefd = 0         !Whether there is a preferred zebra direction
+          solverOptions%mg_zebra_it    = 1         !Number of smoothing passes per zebra line
+          solverOptions%mg_zebra_omega = 1d0       !Relaxation constant per zebra line
+
+          solverOptions%mg_galerkin    =.false.    !Whether to do Galerkin coarsening (true)
                                                    !  or rediscretization (false)
+          solverOptions%mg_debug       =.false.    !Whether to turn on MG graphic debugging
 
 cc          solverOptions%mg_grid_def = grid_params  !Defines default MG grid levels def.
           solverOptions%mg_grid_def => grid_params  !Defines default MG grid levels def.
@@ -253,7 +271,7 @@ c     ###################################################################
 c       Modifies solver definition at depth 'depth' in solver hierarchy
 
           type (solver_unit) :: solver_def
-          integer               :: depth
+          integer :: depth
 
         !Begin
 
@@ -267,7 +285,7 @@ c     ###################################################################
 
 c       Gets solver options at depth 'depth'
 
-          integer    :: depth
+          integer :: depth
           type (solver_unit) :: solver_def
 
         !Begin
